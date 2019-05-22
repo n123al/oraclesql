fa448f0245f18436d7deeddffc61f9a8d42d49ca
Nick <nicholas@hadjimina.com>
2019-05-22 10:19:38 +0300

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var oraclesql_1 = require("./oraclesql");
+exports.sql = oraclesql_1.sql;
+//# sourceMappingURL=index.js.map
